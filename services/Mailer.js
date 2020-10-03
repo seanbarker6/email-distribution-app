75ffeb8b283a61b8f9ae302e6a24410c53cdeba1
Sean Barker <seanbarker6@sky.com>
2020-10-03 13:19:22 +0100

    Users can see survey results

--- services/Mailer.js
+++ services/Mailer.js
@@ -12,7 +12,7 @@ class Mailer extends helper.Mail {
     super();
 
     this.sgApi = sendgrid(keys.sendGridKey);
-    this.from_email = new helper.Email("seanbarker6@sky.com");
+    this.from_email = new helper.Email(keys.senderEmail);
     this.subject = subject;
     this.body = new helper.Content("text/html", content);
     this.recipients = this.formatAddresses(recipients);
